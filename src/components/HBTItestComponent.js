7ba48d23769e4c9466de7e12ba912cc98cbff66d
yungo1468 <yungo1846@gmail.com>
2021-01-09 16:06:25 +0900

    fix: HBTI test 버튼 동작 방식 변경

--- src/components/HBTItestComponent.js
+++ src/components/HBTItestComponent.js
@@ -1,21 +1,17 @@
 import React from "react";
 
-const HBTItestComponent = ({
-  question,
-  onClickFirstAnswer,
-  onClickSecondAnswer,
-}) => {
+const HBTItestComponent = ({ question, onClickNext }) => {
   return (
     <div id="question-container">
       <div id="question">{`Q${question.id}. ${question.question}`}</div>
-      <label onClick={onClickFirstAnswer}>
-        <input type="radio" className="answer-box" id="answer1" name="answer" />
-        {`${question.answer1}`}
-      </label>
-      <label onClick={onClickSecondAnswer}>
-        <input type="radio" className="answer-box" id="answer2" name="answer" />
-        {`${question.answer2}`}
-      </label>
+      <div
+        className="answer-button"
+        onClick={() => onClickNext(true)}
+      >{`${question.answer1}`}</div>
+      <div
+        className="answer-button"
+        onClick={() => onClickNext(false)}
+      >{`${question.answer2}`}</div>
     </div>
   );
 };
--- src/pages/HBTItest.js
+++ src/pages/HBTItest.js
@@ -1,37 +1,27 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { HBTItestQuestions } from "../questions/HBTItestQuestions";
 import HBTItestComponent from "../components/HBTItestComponent";
 import HBTItestResult from "../components/HBTItestResult";
+import "./HBTItest.scss";
 
 const HelchangTest = () => {
   const [currentQuestionId, setCurrentQuestionId] = useState(1);
-  const [answer, setAnswer] = useState(true);
   const [submittedList, setSubmittedList] = useState([]);
 
-  const onClickFirstAnswer = useCallback(() => {
-    setAnswer(true);
-  }, []);
-
-  const onClickSecondAnswer = useCallback(() => {
-    setAnswer(false);
-  }, []);
-
-  const onClickNext = () => {
+  const onClickNext = (answer) => {
     const newSubmittedList = submittedList.concat(answer);
     setSubmittedList(newSubmittedList);
     setCurrentQuestionId(currentQuestionId + 1);
   };
 
   return (
-    <div id="Helchang-test-container">
+    <div id="HBTI-test-container">
       {HBTItestQuestions.length !== currentQuestionId - 1 ? (
         <>
           <HBTItestComponent
             question={HBTItestQuestions[currentQuestionId - 1]}
-            onClickFirstAnswer={onClickFirstAnswer}
-            onClickSecondAnswer={onClickSecondAnswer}
+            onClickNext={onClickNext}
           />
-          <button onClick={onClickNext}>다음</button>
         </>
       ) : (
         <HBTItestResult submittedList={submittedList} />
